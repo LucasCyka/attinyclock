3afcc9294880257d0454415a85a670ece17b9a4f
Steapa <santoslucas347@gmail.com>
2024-08-10 20:05:16 -0300

    Moved files

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-avr-gcc -mmcu=attiny2313 -O2 -o main.o main.c
+avr-gcc -mmcu=attiny2313 -O2 -o hex/main.o source/main.c
 
-avr-objcopy -O ihex -j.text -j.data main.o main.hex
+avr-objcopy -O ihex -j.text -j.data hex/main.o hex/main.hex
 
-avr-size -C -x --mcu=attiny2313 main.o
+avr-size -C -x --mcu=attiny2313 hex/main.o
